7dc687b899d74a3ac1bc10286017428f600143a9
towfeeqfayaz11 <69567257+towfeeqfayaz11@users.noreply.github.com>
2025-02-11 15:09:50 +0900

    Time: 2 ms (88.13%), Space: 55.8 MB (36.94%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self-optimal.java
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self-optimal.java
@@ -1,28 +1,23 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        // using prefix and suffix product array
-        // Approach:
-        //  - prefix array for index i will store product of all elements from start till i-1
-        //  - suffix array for index i will store product of all elements from end till i+1
+        // optimal approach
+        // TC=O(n)
+        // SC=O(1)
 
         int n = nums.length;
-        int[] prefixProd = new int[n];
-        int[] suffixProd = new int[n];
-        int[] result = new int[n]; // stores final result
-        prefixProd[0] = 1; // because there are no elements on left size of index zero, so default product is 1
-        suffixProd[n-1] = 1; // because there are no elements on right size of index n-1, so default product is 1
+        int[] result = new int[n];
 
-        for(int i=1;i<n; i++){
-            prefixProd[i] = prefixProd[i-1] * nums[i-1];
-        }
-        
-        for(int j=n-2; j>=0;j--){
-            suffixProd[j] = suffixProd[j+1] * nums[j+1];
+        // step1: compute prefix product
+        result[0] = 1;
+        for(int i=1; i<n; i++){
+            result[i] = result[i-1] * nums[i-1];
         }
 
-        // final result array as product of prefix and suffix element
-        for(int i=0; i<n; i++){
-            result[i] = prefixProd[i] * suffixProd[i];
+        // step2: compute suffix product and update result
+        int suffixProduct = 1;
+        for(int i=n-1; i>=0; i--){
+            result[i] = result[i] * suffixProduct;
+            suffixProduct = suffixProduct * nums[i];
         }
 
         return result;
